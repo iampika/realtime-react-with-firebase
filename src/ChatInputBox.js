0eec320edaf533d8a5da8c5742a7deb9985814c3
Mahendra Choudhary <jakepintu@gmail.com>
2020-01-04 20:10:34 +0530

    Feat: Saving messages to firebase

--- src/ChatInputBox.js
+++ src/ChatInputBox.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import db from './db'
 
 const ChatInputBox = () => {
   const handleSubmit = event => {
     event.preventDefault()
+    const { value } = event.target.elements[0]
+
+    // db.collection('channels/general/messages')
+
+    db.collection('channels')
+      .doc('random')
+      .collection('messages')
+      .add({
+        text: value,
+        createdAt: new Date()
+      })
+
+    event.target.reset()
   }
 
   return (
