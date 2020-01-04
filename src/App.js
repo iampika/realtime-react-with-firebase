5703c96118f21f198a7f24e1a35ed13ab9e750bf
Mahendra Choudhary <jakepintu@gmail.com>
2020-01-04 18:32:16 +0530

    Add: app boilerplate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,109 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App() {
-  return <div className="App">App</div>
-}
+  const [channels, setChannels] = useState([
+    {
+      id: 'general'
+    }
+  ])
 
+  useEffect(() => {
+    setChannels([
+      ...channels,
+      {
+        id: 'random'
+      }
+    ])
+  }, [])
+
+  return (
+    <div className="App">
+      <div className="Nav">
+        <div className="User">
+          <img
+            className="UserImage"
+            alt="whatever"
+            src="https://placekitten.com/64/64"
+          />
+          <div>
+            <div>Mahendra Choudhary</div>
+            <div>
+              <button type="button" className="text-button">
+                log out
+              </button>
+            </div>
+          </div>
+        </div>
+        <nav className="ChannelNav">
+          {channels.map(channel => (
+            <a href={`/channel/${channel.id}`}># {channel.id}</a>
+          ))}
+        </nav>
+      </div>
+      <div className="Channel">
+        <div className="ChannelMain">
+          <div className="ChannelInfo">
+            <div className="Topic">
+              Topic:{' '}
+              <input
+                className="TopicInput"
+                value="Awesome stuff"
+              />
+            </div>
+            <div className="ChannelName">#general</div>
+          </div>
+          <div className="Messages">
+            <div className="EndOfMessages">
+              That&#39;s every message!
+            </div>
+            <div>
+              <div className="Day">
+                <div className="DayLine" />
+                <div className="DayText">12/6/2018</div>
+                <div className="DayLine" />
+              </div>
+              <div className="Message with-avatar">
+                <div className="Avatar" />
+                <div className="Author">
+                  <div>
+                    <span className="UserName">
+                      Mahendra Choudhary
+                    </span>
+                    <span className="TimeStamp">3:37 PM</span>
+                  </div>
+                  <div className="MessageContent">
+                    Alright, lets do this.
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="Message no-avatar">
+                <div className="MessageContent">works now?</div>
+              </div>
+            </div>
+          </div>
+          <div className="ChatInputBox">
+            <input
+              className="ChatInput"
+              placeholder="Message #general"
+            />
+          </div>
+        </div>
+        <div className="Members">
+          <div>
+            <div className="Member">
+              <div className="MemberStatus offline" />
+              Mahendra Choudhary
+            </div>
+            <div className="Member">
+              <div className="MemberStatus online" />
+              cleverbot
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 export default App
