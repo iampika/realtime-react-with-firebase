1110ee8039447a40f3cfe34d7b65e420852cd9a9
Mahendra Choudhary <jakepintu@gmail.com>
2020-01-05 14:27:29 +0530

    Displaying author on messages

--- src/Messages.js
+++ src/Messages.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import useCollection from './useCollection'
+import { db } from './firebase'
 
 const Messages = () => {
   const messages = useCollection(
@@ -13,28 +14,16 @@ const Messages = () => {
         That&#39;s every message!
       </div>
       {messages.map((message, index) => {
-        return index === 0 ? (
-          <div key={message.id}>
-            <div className="Day">
-              <div className="DayLine" />
-              <div className="DayText">12/6/2018</div>
-              <div className="DayLine" />
-            </div>
-            <div className="Message with-avatar">
-              <div className="Avatar" />
-              <div className="Author">
-                <div>
-                  <span className="UserName">
-                    Mahendra Choudhary
-                  </span>
-                  <span className="TimeStamp">3:37 PM</span>
-                </div>
-                <div className="MessageContent">
-                  {message.text}
-                </div>
-              </div>
-            </div>
-          </div>
+        const previous = messages[index - 1]
+        const showDay = false
+        const showAvatar =
+          !previous || previous.user.id !== message.user.id
+        return showAvatar ? (
+          <FirstMessageFromUser
+            key={message.id}
+            message={message}
+            showDay={showDay}
+          />
         ) : (
           <div key={message.id}>
             <div className="Message no-avatar">
@@ -45,6 +34,56 @@ const Messages = () => {
           </div>
         )
       })}
+    </div>
+  )
+}
+
+const useDoc = path => {
+  const [doc, setDoc] = useState([])
+
+  useEffect(() => {
+    db.doc(path).onSnapshot(doc => {
+      setDoc({
+        ...doc.data(),
+        id: doc.id
+      })
+    })
+  }, [])
+
+  return doc
+}
+
+const FirstMessageFromUser = ({ message, showDay }) => {
+  const author = useDoc(message.user.path)
+
+  return (
+    <div>
+      {showDay && (
+        <div className="Day">
+          <div className="DayLine" />
+          <div className="DayText">12/6/2018</div>
+          <div className="DayLine" />
+        </div>
+      )}
+      <div className="Message with-avatar">
+        <div
+          className="Avatar"
+          style={{
+            backgroundImage: author
+              ? `url("${author.photoURL}")`
+              : ''
+          }}
+        />
+        <div className="Author">
+          <div>
+            <span className="UserName">
+              {author && author.displayName}
+            </span>
+            <span className="TimeStamp">3:37 PM</span>
+          </div>
+          <div className="MessageContent">{message.text}</div>
+        </div>
+      </div>
     </div>
   )
 }
