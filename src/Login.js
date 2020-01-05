ade5f587a79f494e5ad90d60f2c1ff607fa0a1ff
Mahendra Choudhary <jakepintu@gmail.com>
2020-01-05 13:57:48 +0530

    Login Error handling

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useState, Fragment } from 'react'
 import { firebase } from './firebase'
 
 function Login() {
+  const [authError, setAuthError] = useState(null)
+
   const handleSignIn = async () => {
     const provider = new firebase.auth.GoogleAuthProvider()
-    await firebase.auth().signInWithPopup(provider)
+    try {
+      await firebase.auth().signInWithPopup(provider)
+    } catch (error) {
+      setAuthError(error)
+    }
   }
+
   return (
     <div className="Login">
       <h1>Chat!</h1>
       <button onClick={handleSignIn} type="button">
         Sign in with google
       </button>
+      {authError && (
+        <Fragment>
+          <p>Sorry, there was a problem.</p>
+          <p>
+            <i>{authError.message}</i>
+          </p>
+          <p>Please try again</p>
+        </Fragment>
+      )}
     </div>
   )
 }
